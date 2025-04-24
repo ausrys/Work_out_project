3576de2c243986f3c1a054ba6a4892dbfeb86bb2
Ausrys Work <ausrys.mikoliunas@orioninc.com>
2025-04-24 13:18:25 +0300

    app models

---
+++ workout_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import City, SportsmanLevel, Program, Sportsman
+# Register your models here.
+admin.site.register(City)
+admin.site.register(SportsmanLevel)
+admin.site.register(Program)
+admin.site.register(Sportsman)
---
+++ workout_app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class City(models.Model):
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.city
+
+
+class SportsmanLevel(models.Model):
+    level = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.level
+
+
+class Program(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=200)
+    program_description = models.TextField()
+    levels = models.ForeignKey(
+        SportsmanLevel, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.date})"
+
+
+class Sportsman(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    level = models.ForeignKey(
+        SportsmanLevel, on_delete=models.SET_NULL, null=True, blank=True)
+    city = models.ForeignKey(
+        City, on_delete=models.SET_NULL, null=True, blank=True)
+    program = models.ForeignKey(
+        Program, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
